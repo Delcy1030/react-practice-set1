818ce3dd6d0f960fb4b48c5262ad0f8dd975ab04
[delcy1030] <[delcymary69@gmail.com]>
2024-09-11 22:36:33 +0530

    lets start

---
+++ customReact/customreact.js
@@ -0,0 +1,11 @@
+const reactElement= {
+    type: 'a',
+    props:{
+        href: 'https://google.com',
+        target: '_black'
+    },
+    children: 'click me to visit'
+}
+
+const mainContainer = document.querySelector('#root')
+
