bf661dd63d50aa05751b120ddb7172e458f75001
[delcy1030] <[delcymary69@gmail.com]>
2024-09-13 20:02:04 +0530

    dgg

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  let counter=5
+
+  return (
+    <>
+      
+      <h1>hello delcy</h1>
+      <h2>hello vishal</h2>
+      <h3>counter value : {counter}</h3>
+
+      <button>add value</button>
+      <br />
+      <br />
+      <button>decrease button</button>
+    </>
+  )
+}
+
+export default App
